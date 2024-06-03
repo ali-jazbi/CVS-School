6699c31b2894dceed795a980249ff4a33cf5b125
Ali Jazbi <ali.jazbi@yahoo.com>
2024-06-03 20:00:34 +0330

    add first files

--- script/loader.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('main').style.display = 'none';
-window.addEventListener('load', function(){
-        document.querySelector('.loader').classList.add('hide');
-        document.querySelector('main').style.display = 'block'; 
-});
