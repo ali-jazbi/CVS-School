ba3b4c0c9c8b17e92f15adf21b6f4aa6c0d76d71
Ali Jazbi <ali.jazbi@yahoo.com>
2024-12-27 16:55:13 +0330

    add new header and footer to another pages

--- scripts/megamenu.js
+++ scripts/megamenu.js
@@ -1,7 +1,7 @@
 // Define images for each section
 const sectionImages = {
 	web: "https://itspectrumsolutions.com/wp-content/uploads/2024/04/1_pE2fOVDikEUwiQJlh4ggzg.jpeg",
-	apps: "https://riseuplabs.com/wp-content/uploads/2021/07/mobile-application-development-guidelines-riseuplabs.jpg",
+	apps: "https://cdn.nody.ir/files/2021/11/02/nody-%D8%B9%DA%A9%D8%B3-%D8%A7%D8%B2-%DA%A9%D9%84%D8%A7%D8%B3-%DA%A9%D9%86%DA%A9%D9%88%D8%B1-1635835098.jpg",
 };
 
 // Add hover events for services section
